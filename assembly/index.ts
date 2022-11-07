2460b99809d59c734138b9a5e1d7366b2853bceb
Dave Roberts <dave@tac-llc.com>
2022-11-07 19:23:19 +0000

    20221107.3

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,19 @@
-///sample dapp project in assemblyScript
+///sample dapp project in assemblyScript on NEAR blockchain
 /// imports
 import { PersistentUnorderedMap} from "near-sdk-as";
 
 // variables
-const courses = new PersistentUnorderedMap<string, string>("COURSES");
+export const courses = new PersistentUnorderedMap<string, string>("COURSES");
+
+// must be exported as it comes from outside the smart contract
+export function addCourse(id: string, name: string): void { 
+
+  courses.set(id, name);// exclusive to PersistentUnorderedMap
+
+}
+
+export function getCourse(id: string): string | null { 
+
+  return courses.get(id);
+
+}
